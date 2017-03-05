432973efaba9a3c6502253b790060e2ca09da8b9
Colton Ehrman <cjames615@hotmail.com>
2017-03-05 16:15:15 -0600

    Minify materialize js

--- gulp/build.js
+++ gulp/build.js
@@ -5,37 +5,36 @@ const rev = require('gulp-rev');
 const cssnano = require('gulp-cssnano');
 const uglify = require('gulp-uglify');
 
-gulp.task('deleteDocsFolder', () => {
-  del('./docs/**');
-});
+gulp.task('deleteDocsFolder', () => (
+  del('./docs/**')
+));
 
-gulp.task('copyGeneralFiles', ['deleteDocsFolder'], () => {
+gulp.task('copyGeneralFiles', ['deleteDocsFolder'], () => (
   gulp.src([
     './app/**/*',
     '!./app/index.html',
-    '!./app/assets/',
-    '!./app/assets/**/*',
     '!./app/sass/',
     '!./app/sass/**/*',
     '!./app/js/',
     '!./app/js/**/*',
+    '!./app/lib/materialize.js',
     '!./app/temp/',
     '!./app/temp/**/*',
   ])
-  .pipe(gulp.dest('./docs/'));
-});
+  .pipe(gulp.dest('./docs/'))
+));
 
-gulp.task('useminTrigger', ['deleteDocsFolder'], () => {
-  gulp.start('usemin');
-});
+gulp.task('useminTrigger', ['deleteDocsFolder'], () => (
+  gulp.start('usemin')
+));
 
-gulp.task('usemin', ['sass', 'scripts'], () => {
+gulp.task('usemin', ['sass', 'scripts'], () => (
   gulp.src('./app/index.html')
     .pipe(usemin({
       css: [() => rev(), () => cssnano()],
       js: [() => rev(), () => uglify()],
     }))
-    .pipe(gulp.dest('./docs/'));
-});
+    .pipe(gulp.dest('./docs/'))
+));
 
 gulp.task('build', ['deleteDocsFolder', 'useminTrigger', 'copyGeneralFiles']);
--- gulp/dev.js
+++ gulp/dev.js
@@ -17,10 +17,10 @@ gulp.task('dev', ['sass', 'scripts'], () => {
   gulp.watch('./app/js/**/*.js', ['reloadScripts']);
 });
 
-gulp.task('injectCss', ['sass'], () => {
+gulp.task('injectCss', ['sass'], () => (
   gulp.src('./app/temp/styles.css')
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+));
 
 gulp.task('reloadScripts', ['scripts'], () => {
   browserSync.reload();
