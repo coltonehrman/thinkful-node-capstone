a71fb2ad7a033742ed91a821fc0f0c9d4f864a39
Colton Ehrman <cjames615@hotmail.com>
2017-04-11 19:20:07 -0500

    Fix webpack dependency - again

--- server/middleware/webpackMiddleware.js
+++ server/middleware/webpackMiddleware.js
@@ -1,11 +1,11 @@
 /* eslint global-require: 0 */
-const webpackConfig = require('../../config/webpack.config.development');
 const config = require('../config');
 
 let compiler;
 let webpackMiddleware;
 
 if (config.env !== config.prod) {
+  const webpackConfig = require('../../config/webpack.config.development');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
   compiler = webpack(webpackConfig);
